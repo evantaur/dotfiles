7aabb29307019d2be0860430c2725085b453af1d
evantaur <jani.lehtima@gmail.com>
2024-08-24 15:23:40 +0000

    Some QoL changes

--- bash/scripts/install.sh
+++ bash/scripts/install.sh
@@ -10,8 +10,9 @@
 wget --show-progress "https://github.com/evantaur/dotfiles/archive/refs/heads/main.tar.gz" -O /tmp/dotfiles.tar.gz && \
 cd /tmp/ && \
 tar xvf dotfiles.tar.gz && \
+
 cp -r dotfiles-main/bash $HOME/.config/ && \
-[[ -e $HOME/.bashrc ]] && mv $HOME/.bashrc $HOME/._backup_bashrc
+[[ -e $HOME/.bashrc && ! -L $HOME/.bashrc ]] && mv $HOME/.bashrc $HOME/._backup_bashrc
 cd $HOME
 ln -s $HOME/.config/bash/bashrc ~/.bashrc && \
 bash -l
