281815c08dc39a2cea8ea95012f879cdbf84ae25
evantaur <jani.lehtima@gmail.com>
2024-08-25 19:46:34 +0000

    updated installer to include git

--- bash/scripts/install.sh
+++ bash/scripts/install.sh
@@ -7,14 +7,25 @@
 # Create dirpath if it doesn't exist
 [[ -z $HOME/.config/bash ]] || mkdir -p bash
 
-wget --show-progress "https://github.com/evantaur/dotfiles/archive/refs/heads/main.tar.gz" -O /tmp/dotfiles.tar.gz && \
-cd /tmp/ && \
-tar xvf dotfiles.tar.gz && \
-
-cp -r dotfiles-main/bash $HOME/.config/ && \
+# Use Git if found else try wget
+if command -v git &> /dev/null ; then
+  cd /tmp
+  git clone -n --depth=1 --filter=tree:0 https://github.com/evantaur/dotfiles.git && \
+  cd dotfiles && \
+  git sparse-checkout set --no-cone bash && \
+  git checkout
+  mv /tmp/dotfiles /tmp/dotfiles-main
+else
+  wget --show-progress "https://github.com/evantaur/dotfiles/archive/refs/heads/main.tar.gz" -O /tmp/dotfiles.tar.gz && \
+  cd /tmp/ && \
+  tar xvf dotfiles.tar.gz
+fi && \
+cp -r /tmp/dotfiles-main/bash $HOME/.config/ && \
 [[ -e $HOME/.bashrc && ! -L $HOME/.bashrc ]] && mv $HOME/.bashrc $HOME/._backup_bashrc
 cd $HOME
 [[ -e ~/.bashrc ]] || ln -s $HOME/.config/bash/bashrc ~/.bashrc
+rm -rf /tmp/dotfiles-main
+[[ -e "/tmp/dotfiles.tar.gz" ]] && rm /tmp/dotfiles.tar.gz
 bash -l
 
 
