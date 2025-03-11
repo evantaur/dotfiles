fad431f0c2576d0aba2c14d45353b7ac63839191
evantaur <jani.lehtima@gmail.com>
2025-03-11 00:20:59 +0000

    Added random color/emoji to install script

--- bash/scripts/install.sh
+++ bash/scripts/install.sh
@@ -31,6 +31,48 @@ cd $HOME
 [[ -e ~/.vim ]] || ln -s $HOME/.config/vim/vim ~/.vim
 
 
+random_emoji(){
+    # Unicode emoji ranges (Hexadecimal)
+    ranges=(
+        #"1F600 1F64F"  # Emoticons
+        "1F300 1F5FF"  # Misc symbols & pictographs
+        #"1F680 1F6FF"  # Transport & map
+        "1F900 1F9FF"  # Supplemental symbols & pictographs
+        #"1FA70 1FAFF"  # Symbols & pictographs extended
+    )
+
+
+    # Pick a random range
+    range_index=$((RANDOM % ${#ranges[@]}))
+    range=(${ranges[$range_index]})
+
+    # Pick a random codepoint in the selected range
+    random_code=$(( (0x${range[0]}) + RANDOM % ( (0x${range[1]}) - (0x${range[0]}) ) ))
+
+    # Convert and print emoji
+    printf "\U$(printf "%X" $random_code)\n"
+
+}
+
+# Function to generate a random color, ensuring it's not too dark
+generate_color() {
+    min_brightness=10  # Adjust this for brightness (0-255)
+
+    # Generate R, G, B values ensuring they're above the threshold
+    R=$(( RANDOM % (256 - min_brightness) + min_brightness ))
+    G=$(( RANDOM % (256 - min_brightness) + min_brightness ))
+    B=$(( RANDOM % (256 - min_brightness) + min_brightness ))
+
+    # Convert to HEX
+    printf "#%02X%02X%02X\n" "$R" "$G" "$B"
+}
+
+
+# Man the hostcolor and host icon if not present
+[[ -f ~/.config/bash/.hostcolor ]] || generate_color > $HOME/.config/bash/.hostcolor
+[[ -f ~/.config/bash/.hosticon ]] || random_emoji > $HOME/.config/bash/.hostcolor
+
+
 rm -rf /tmp/dotfiles-main
 [[ -e "/tmp/dotfiles.tar.gz" ]] && rm /tmp/dotfiles.tar.gz
 source ~/.bashrc
